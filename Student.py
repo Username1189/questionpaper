1b356d5eae25b22055724f6788b0c3bf90d9a9fa
Username1183 <sanjaymuthu12@gmail.com>
2021-12-09 20:13:16 +0530

    Commit 17

--- Student.py
+++ Student.py
@@ -161,7 +161,7 @@ class Student:
                 else:
                     options = [st.radio('Answer:', question.choices)]
         else:
-            if self.state.file["MultipleAnswers"][self.state.question_number].lower() == "yes":
+            if str(self.state.file["Ans"][self.state.question_number]).find(",") != -1:
                 options = []
                 for j in range(1, 5):
                     if st.checkbox(question.choices[j], key=j):
