4477b692cb38ef88897fc7221b7b40c9d16f7db6
Username1183 <sanjaymuthu12@gmail.com>
2021-12-09 14:03:14 +0530

    Commit 15

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from Student import Student
 
 def main():
     st.set_page_config(page_title="MCQ question paper")
+    st.markdown(""" <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    </style> """, unsafe_allow_html=True)
     student = Student()
     student.show()
 
